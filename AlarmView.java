5d1cd22934f49eb3a7f0d1757655ee4f219e1875
ZaleJ <947703213@qq.com>
2016-04-21 08:28:11 -0500

    Added files via upload

--- AlarmView.java
+++ AlarmView.java
@@ -2,18 +2,16 @@ package com.jzlclock.jzl.jzlclock;
 
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by Administrator on 2016/4/20 0020.
@@ -41,7 +39,9 @@ public class AlarmView extends LinearLayout{
         adapter = new ArrayAdapter<AlarmData>(getContext(), android.R.layout.simple_list_item_1);
         lvAlarmList.setAdapter(adapter);
 
-        adapter.add(new AlarmData(System.currentTimeMillis()));
+        readSaveAlarmList();     // 读取已经保存好的闹钟数据
+
+        //adapter.add(new AlarmData(System.currentTimeMillis()));
 
         btnAddAlarm.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -67,12 +67,42 @@ public class AlarmView extends LinearLayout{
                 }
 
                 adapter.add(new AlarmData(calendar.getTimeInMillis()));
+                saveAlarmList();     // 保存新建闹钟数据
             }
         }, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true).show();
+    }
+    private void saveAlarmList(){
+        SharedPreferences.Editor editor = getContext().getSharedPreferences(AlarmView.class.getName(), Context.MODE_PRIVATE).edit();
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < adapter.getCount(); i++) {
+            sb.append(adapter.getItem(i).getTime()).append(","); //传入数据、时间， 多个中间用逗号隔开
+        }
+
+        String content = sb.toString().substring(0, sb.length()-1);
+
+        editor.putString(KEY_ALARM_LIST, content);     // 输出除最后一个类型数据
+
+        System.out.println(content);
+
+        editor.commit();  // 提交数据
+    }
+
+    private void readSaveAlarmList() {
+        SharedPreferences sp = getContext().getSharedPreferences(AlarmView.class.getName(), Context.MODE_PRIVATE);
+        String content = sp.getString(KEY_ALARM_LIST, null);
+
+        if (content!=null) {
+            String[] timeStrings = content.split(",");
+            for (String string : timeStrings) {
+                adapter.add(new AlarmData(Long.parseLong(string)));
+            }
+        }
     }
 
     private Button btnAddAlarm;
     private ListView lvAlarmList;
+    private static final String KEY_ALARM_LIST = "alarmList";
     private ArrayAdapter<AlarmData> adapter;
 
     private static class AlarmData{
